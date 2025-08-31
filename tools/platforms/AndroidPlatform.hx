933be4deca8e6e38a53c4df8c7615538aa1f13df
Giuppe <giuppe@users.noreply.github.com>
2025-08-31 21:05:03 +0200

    Allow setting Gradle properties in project.xml (1859)
    * allow setting gradle jvmargs in project.xml
    
    * Make gradle.properties fully configurable from project.xml.
    
    ---------
    
    Co-authored-by: Joseph Cloutier <player3.14@gmail.com>
    
    Cherry picked from: e439f0d0ae04220fd5e54a703b1c992907f3964e

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -477,6 +477,7 @@ class AndroidPlatform extends PlatformTarget
 		context.ANDROID_GRADLE_PLUGIN = project.config.getString("android.gradle-plugin", "8.7.3");
 		context.ANDROID_USE_ANDROIDX = project.config.getString("android.useAndroidX", "true");
 		context.ANDROID_ENABLE_JETIFIER = project.config.getString("android.enableJetifier", "false");
+		context.ANDROID_GRADLE_PROPERTIES = project.config.getKeyValueArray("android.gradle-properties");
 
 		context.ANDROID_APPLICATION = project.config.getKeyValueArray("android.application", {
 			"android:label": project.meta.title,
